b1dcedfcd689d6f51a5ad02aac8240f3e089ac17
Artur <artur.m99@wp.pl>
2020-11-20 22:18:13 +0100

    Implemented personal exceptions in case they are useful

---
+++ app/src/main/java/com/mobilki/covidapp/exceptions/emptyResponseBodyException.java
@@ -0,0 +1,7 @@
+package com.mobilki.covidapp.exceptions;
+
+public class emptyResponseBodyException extends Exception {
+    public emptyResponseBodyException(String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/com/mobilki/covidapp/exceptions/incorrectRequestException.java
@@ -0,0 +1,7 @@
+package com.mobilki.covidapp.exceptions;
+
+public class incorrectRequestException extends Exception {
+    public incorrectRequestException(String message) {
+        super(message);
+    }
+}
