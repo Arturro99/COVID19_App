37171b31c91a607c84743878ba071a2f6d9f53f2
Artur <artur.m99@wp.pl>
2020-11-30 23:03:23 +0100

    Improved displaying movies programmatically. Added a particular view for concrete movie.

--- app/src/main/java/com/mobilki/covidapp/EntertainmentActivity.java
+++ app/src/main/java/com/mobilki/covidapp/EntertainmentActivity.java
@@ -1,11 +1,14 @@
 package com.mobilki.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -32,14 +35,26 @@ public class EntertainmentActivity extends AppCompatActivity {
     LinearLayout gamesLayout;
     LayoutInflater inflater;
 
-    ImageButton mainFilmPhoto;
-    ImageButton mainBookPhoto;
-    ImageButton mainGamePhoto;
 
-    TextView []filmTitleTxtList = new TextView[10];
+    TextView []filmTitleList = new TextView[10];
+    TextView []filmGenresList = new TextView[10];
+
     ImageButton []filmPhotosList = new ImageButton[10];
+
     TextView []filmDirectorList = new TextView[10];
+    TextView []filmDirectorTxtList = new TextView[10];
+
     TextView []filmReleaseYearList = new TextView[10];
+    TextView []filmReleaseYearTxtList = new TextView[10];
+
+    TextView []filmDurationList = new TextView[10];
+    TextView []filmDurationTxtList = new TextView[10];
+
+    TextView []filmRatingList = new TextView[10];
+    TextView []filmRatingTxtList = new TextView[10];
+
+
+    ConstraintLayout []filmConstraintLayoutList = new ConstraintLayout[10];
 
     private FilmDatabaseApi imdbApi;
     private List synchedList = Collections.synchronizedList(new LinkedList<>());
@@ -58,23 +73,157 @@ public class EntertainmentActivity extends AppCompatActivity {
 
         inflater = LayoutInflater.from(this);
 
+
         int filmTitleInitiateId = 1000;
+
         int filmReleaseYearInitiateId = 2000;
-        int filmPhotoInitiateId = 2000;
+        int filmReleaseYearTxtInitiateId = 2500;
+
+        int filmPhotoInitiateId = 3000;
+
+        int filmDirectorInitiateId = 4000;
+        int filmDirectorTxtInitiateId = 4500;
+
+        int filmDurationInitiateId = 5000;
+        int filmDurationTxtInitiateId = 5500;
+
+        int filmRatingInitiateId = 6000;
+        int filmRatingTxtInitiateId = 6500;
+
+        int filmGenresInitiateId = 7000;
+
+        int filmConstraintLayoutInitiateId = 10000;
+
+        ConstraintLayout constraintLayout;
+
+
 
         for (int i = 0; i < 10; i++) {
             View anotherLayout = inflater.inflate(R.layout.film_overview, null, true);
             filmsLayout.addView(anotherLayout);
-            filmTitleTxtList[i] = findViewById(R.id.filmTitleTxt);
-            //filmPhotosList[i] = findViewById(R.id.mainFilmPhoto);
-//            filmDirectorList[i] = findViewById(R.id.filmDirector);
-            filmReleaseYearList[i] = findViewById(R.id.filmReleaseYear);
 
-            filmTitleTxtList[i].setId(filmTitleInitiateId + i);
-            //filmPhotosList[i].setId(R.id.mainFilmPhoto + i);
-//            filmDirectorList[i].setId(R.id.filmDirector + i + 1);
+            filmConstraintLayoutList[i] = findViewById(R.id.movieConstraintLayout);
+            filmTitleList[i] = findViewById(R.id.filmTitle);
+            filmPhotosList[i] = findViewById(R.id.mainFilmPhoto);
+            filmGenresList[i] = findViewById(R.id.filmGenres);
+
+            filmDirectorList[i] = findViewById(R.id.filmDirector);
+            filmDirectorTxtList[i] = findViewById(R.id.filmDirectorTxt);
+
+            filmRatingList[i] = findViewById(R.id.filmRating);
+            filmRatingTxtList[i] = findViewById(R.id.filmRatingTxt);
+
+            filmReleaseYearList[i] = findViewById(R.id.filmReleaseDate);
+            filmReleaseYearTxtList[i] = findViewById(R.id.filmReleaseDateTxt);
+
+            filmDurationList[i] = findViewById(R.id.filmDuration);
+            filmDurationTxtList[i] = findViewById(R.id.filmDurationTxt);
+
+
+            filmConstraintLayoutList[i].setId(filmConstraintLayoutInitiateId + i);
+            filmTitleList[i].setId(filmTitleInitiateId + i);
+            filmPhotosList[i].setId(filmPhotoInitiateId + i);
+            filmGenresList[i].setId(filmGenresInitiateId + i);
+
             filmReleaseYearList[i].setId(filmReleaseYearInitiateId + i);
-            //
+            filmReleaseYearTxtList[i].setId(filmReleaseYearTxtInitiateId + i);
+
+            filmDirectorList[i].setId(filmDirectorInitiateId + i);
+            filmDirectorTxtList[i].setId(filmDirectorTxtInitiateId + i);
+
+            filmDurationList[i].setId(filmDurationInitiateId + i);
+            filmDurationTxtList[i].setId(filmDurationTxtInitiateId + i);
+
+            filmRatingList[i].setId(filmRatingInitiateId + i);
+            filmRatingTxtList[i].setId(filmRatingTxtInitiateId + i);
+
+            constraintLayout = findViewById(filmConstraintLayoutInitiateId + i);
+            constraintLayout.setMinWidth(1200);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+
+            //IMAGE
+            constraintSet.connect(filmPhotoInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.START);
+            constraintSet.connect(filmPhotoInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+
+            //TITLE
+            constraintSet.connect(filmTitleInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmTitleInitiateId + i, ConstraintSet.END, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmTitleInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.START);
+            constraintSet.connect(filmTitleInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.BOTTOM);
+
+
+            //DIRECTOR TXT
+            constraintSet.connect(filmDirectorTxtInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmDirectorTxtInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDirectorTxtInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDirectorTxtInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmDirectorTxtInitiateId + i, 0.05f);
+
+            //DIRECTOR
+            constraintSet.connect(filmDirectorInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmDirectorInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDirectorInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDirectorInitiateId + i, ConstraintSet.TOP, filmDirectorTxtInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.setVerticalBias(filmDirectorInitiateId + i, 0.01f);
+
+            //RELEASE YEAR TXT
+            constraintSet.connect(filmReleaseYearTxtInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmReleaseYearTxtInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmReleaseYearTxtInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmReleaseYearTxtInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmReleaseYearTxtInitiateId + i, 0.3f);
+            constraintSet.setHorizontalBias(filmReleaseYearTxtInitiateId + i, 0.1f);
+
+            //RELEASE YEAR
+            constraintSet.connect(filmReleaseYearInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmReleaseYearInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmReleaseYearInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmReleaseYearInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmReleaseYearInitiateId + i, 0.3f);
+            constraintSet.setHorizontalBias(filmReleaseYearInitiateId + i, 0.6f);
+
+            //DURATION TXT
+            constraintSet.connect(filmDurationTxtInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmDurationTxtInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDurationTxtInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDurationTxtInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmDurationTxtInitiateId + i, 0.4f);
+            constraintSet.setHorizontalBias(filmDurationTxtInitiateId + i, 0.1f);
+
+            //DURATION
+            constraintSet.connect(filmDurationInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmDurationInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDurationInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmDurationInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmDurationInitiateId + i, 0.4f);
+            constraintSet.setHorizontalBias(filmDurationInitiateId + i, 0.6f);
+
+            //RATINGS TXT
+            constraintSet.connect(filmRatingTxtInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmRatingTxtInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmRatingTxtInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmRatingTxtInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmRatingTxtInitiateId + i, 0.5f);
+            constraintSet.setHorizontalBias(filmRatingTxtInitiateId + i, 0.1f);
+
+            //RATINGS
+            constraintSet.connect(filmRatingInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmRatingInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmRatingInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmRatingInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmRatingInitiateId + i, 0.5f);
+            constraintSet.setHorizontalBias(filmRatingInitiateId + i, 0.6f);
+
+            //Genres
+            constraintSet.connect(filmGenresInitiateId + i, ConstraintSet.BOTTOM, filmConstraintLayoutInitiateId + i, ConstraintSet.BOTTOM);
+            constraintSet.connect(filmGenresInitiateId + i, ConstraintSet.END, filmConstraintLayoutInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmGenresInitiateId + i, ConstraintSet.START, filmPhotoInitiateId + i, ConstraintSet.END);
+            constraintSet.connect(filmGenresInitiateId + i, ConstraintSet.TOP, filmPhotoInitiateId + i, ConstraintSet.TOP);
+            constraintSet.setVerticalBias(filmGenresInitiateId + i, 0.8f);
+            constraintSet.setHorizontalBias(filmGenresInitiateId + i, 0.1f);
+
+            constraintSet.applyTo(constraintLayout);
         }
 
         imdbApi = new ImdbApi();
@@ -84,7 +233,8 @@ public class EntertainmentActivity extends AppCompatActivity {
 
     private void start() {
         imdbApi.getGenres();
-        imdbApi.getTopRatedOrPopularFilms(false);
+        imdbApi.getTopRatedOrPopularFilms(true);
+
         notificationsSettingsBtn.setOnClickListener(view -> {
 //            ExecutorService executorService = Executors.newFixedThreadPool(10);
 //            executorService.submit((Runnable) imdbApi);
@@ -93,11 +243,13 @@ public class EntertainmentActivity extends AppCompatActivity {
         });
         preferencesBtn.setOnClickListener(view -> {
             for (int i = 0; i < 10; i++) {
-                filmTitleTxtList[i].setText(imdbApi.getFilms().get(i).getTitle());
+                filmTitleList[i].setText(imdbApi.getFilms().get(i).getTitle());
                 filmReleaseYearList[i].setText(String.valueOf(imdbApi.getFilms().get(i).getDateOfRelease()));
-                //filmPhotosList[i].setImageResource(Uri.parse(imdbApi.getFilms().get(i).getImageUrl()));
-                //Picasso.get().load(imdbApi.getFilms().get(i).getImageUrl()).into(filmPhotosList[i]);
-                //filmDirectorList[i].setText(Uri.parse(`imdbApi.getFilms().get(i).get()));
+                filmDurationList[i].setText(String.valueOf(imdbApi.getFilms().get(i).getDuration()));
+                filmRatingList[i].setText(String.valueOf(imdbApi.getFilms().get(i).getRatings()));
+                filmGenresList[i].setText(String.valueOf(imdbApi.getFilms().get(i).getGenres()));
+                //filmDirectorList[i].setText(String.valueOf(imdbApi.getFilms().get(i).getDirector()));
+                Picasso.get().load(imdbApi.getFilms().get(i).getImageUrl()).into(filmPhotosList[i]);
             }
         });
     }
---
+++ app/src/main/java/com/mobilki/covidapp/EntertainmentDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.mobilki.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntertainmentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entertainment_details);
+    }
+}
