c69362db81bf09aa80e8d3d8939c48cdb98b461b
Artur <artur.m99@wp.pl>
2021-01-13 23:44:32 +0100

    Implement locale

--- app/src/main/java/com/mobilki/covidapp/MainActivity.java
+++ app/src/main/java/com/mobilki/covidapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.mobilki.covidapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -22,6 +28,11 @@ import com.mobilki.covidapp.entertainment.EntertainmentActivity;
 import com.mobilki.covidapp.health.HealthActivity;
 import com.mobilki.covidapp.health.HealthForm;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
 public class MainActivity extends AppCompatActivity {
 
     Button healthBtn;
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     Button emergencyNumbersBtn;
     Button settingsBtn;
     Button mLogoutBtn;
+    Button polish;
+    Button english;
 
     TextView curiosities, resendVerificationTxt;
     Button resendVerification;
@@ -37,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
     FirebaseFirestore firestore;
 
     SharedPreferences settings;
+    DocumentReference documentReference;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         emergencyNumbersBtn = findViewById(R.id.emergencyNumbersBtn);
         settingsBtn = findViewById(R.id.settingsBtn);
         mLogoutBtn = findViewById(R.id.logout);
+        polish = findViewById(R.id.polish);
+        english = findViewById(R.id.english);
 
         curiosities = findViewById(R.id.mainCuriositiesTxt);
         settings = getSharedPreferences(getResources().getString(R.string.shared_preferences),0);
@@ -60,9 +77,26 @@ public class MainActivity extends AppCompatActivity {
         resendVerificationTxt = findViewById(R.id.resendVerificationTxt);
         user = firebaseAuth.getCurrentUser();
 
+        documentReference = firestore.collection("users").document(user.getUid());
+        documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
+            polish.setVisibility(Optional.ofNullable(documentSnapshot.getString("language")).orElse("english").equals("polish") ? View.GONE : View.VISIBLE);
+            english.setVisibility(polish.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        });
+
+        if (user.isEmailVerified()) {
+            DocumentReference documentReference = firestore.collection("users").document(user.getUid());
+            documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
+                if (documentSnapshot != null)
+                    curiosities.setText("Hello, " + documentSnapshot.getString("name"));
+                else
+                    curiosities.setText("Hello, unknown");
+            });
+        }
+
         start();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("SetTextI18n")
     private void start() {
         healthBtn.setOnClickListener(view -> {
@@ -77,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "E-mail not verified", Toast.LENGTH_SHORT).show();
             }
         });
-//        healthBtn.setOnClickListener(view -> startActivity(new Intent(this, HealthActivity.class)));
         entertainmentBtn.setOnClickListener(view -> {
             if (user.isEmailVerified()) {
                 startActivity(new Intent(this, EntertainmentActivity.class));
@@ -98,20 +131,43 @@ public class MainActivity extends AppCompatActivity {
                         .addOnFailureListener(aVoid -> Toast.makeText(getApplicationContext(), "E-mail not sent (" + aVoid.getMessage() +")", Toast.LENGTH_SHORT).show());
             });
         }
-        else {
-            DocumentReference documentReference = firestore.collection("users").document(user.getUid());
-            documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
-                if (documentSnapshot != null)
-                    curiosities.setText("Hello, " + documentSnapshot.getString("name"));
-                else
-                    curiosities.setText("Hello, unknown");
-            });
-        }
+
+        polish.setOnClickListener(view -> setLocale("pl"));
+        english.setOnClickListener(view -> setLocale("en"));
     }
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setLocale(String localCode) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        Configuration conf = getResources().getConfiguration();
+        conf.setLocale(new Locale(localCode.toLowerCase()));
+        getResources().updateConfiguration(conf, metrics);
+
+        setFlagsVisibility(localCode);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setFlagsVisibility(String localCode) {
+        Map<String, Object> locale = new HashMap<>();
+        if (localCode.equals("pl")) {
+            locale.put("language", "polish");
+            polish.setVisibility(View.GONE);
+            english.setVisibility(View.VISIBLE);
+        }
+        else if (localCode.equals("en")) {
+            locale.put("language", "english");
+            polish.setVisibility(View.VISIBLE);
+            english.setVisibility(View.GONE);
+        }
+        documentReference.set(locale);
     }
 }
